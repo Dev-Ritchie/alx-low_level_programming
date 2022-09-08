215896c9169959666c59d02f770c144705a0f3b6
Dev-Ritchie <Ogbonnayaritchie@gmail.com>
2022-09-08 10:06:52 -0700

    Baby

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * main - program execution starts here
+ * Description: this is the main function
+ * Return: an integer to signify success or fail
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59, 1, stderr);
 	return (1);
 }
