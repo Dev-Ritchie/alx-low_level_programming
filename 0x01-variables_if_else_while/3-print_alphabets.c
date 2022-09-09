4fa32c57dee4bf7ce7ad2783eb45a6b3a1e03273
Dev-Ritchie <Ogbonnayaritchie@gmail.com>
2022-09-09 07:57:26 -0700

    Word

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
+/**
+ *main - print lowercase and uppercase a-zA-Z using putchar
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
+	char lower = 'a';
+	char upper = 'A';
 
-char upper='A';
-char lower='a';
+	while (lower <= 'z') /*print lowercases a-z*/
+	{
+		putchar(lower);
+		lower++;
+	}
 
-while (lower<='z')
-{
-putchar (lower);
-lower++;
+	while (upper <= 'Z') /*print uppercase A-Z*/
+	{
+		putchar(upper);
+		upper++;
+	}
 
-}
+	putchar('\n');
 
-while (upper<='Z')
-{
-putchar (upper);
-upper++;
-}
-
-putchar('\n');
-
-return (0);
-
+	return (0);
 }
