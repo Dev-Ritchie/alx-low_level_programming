5ac9b2f6bbdab8a079c6fee0cfac0c1bab31df59
Dev-Ritchie <Ogbonnayaritchie@gmail.com>
2022-09-14 09:14:57 -0700

    Why Lord

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
@@ -16,3 +21,4 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
+}
