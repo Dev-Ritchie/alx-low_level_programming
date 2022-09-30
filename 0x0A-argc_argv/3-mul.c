205f7f2766b5f43dd551ca05eb1b0ea8165d65f3
Dev-Ritchie <Ogbonnayaritchie@gmail.com>
2022-09-30 16:53:55 +0000

    Horny

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,13 +5,13 @@
 int main(int argc, char *argv[])
 {
 	int m1, m2;
-	m1 = atoi(argv[1]);
-	m2 = atoi(argv[2]);
+	m1 = (argv[1]);
+	m2 = (argv[2]);
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", m1 * m2);
+	printf("%d\n", atoi(m1) * atoi(m2));
 	return (0);
 }
