345d4cd0a591c183ea02c31759d16e9ee502b564
Dev-Ritchie <Ogbonnayaritchie@gmail.com>
2022-10-03 15:02:47 +0000

    Work

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,37 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
-  * str_concat - Concatenates two strings of any size
-  * @s1: the first string to concatenate
-  * @s2: the second string to concatenate
-  *
-  * Return: the two strings concatenated
-  */
-char *str_concat(char *s1, char *s2)
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string to duplicate
+ * Return: pointer to duplicated string in allocated memory
+ */
+
+char *_strdup(char *str)
 {
-	int i = 0, j = 0, k = 0, l = 0;
-	char *s;
+	char *duplicate_str;
+	int i = 0, len = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-
-	while (s2[j])
-		j++;
-
-	l = i + j;
-	s = malloc((sizeof(char) * l) + 1);
-
-	if (s == NULL)
+	if (str == NULL) /* validate str input */
 		return (NULL);
 
-	j = 0;
+	while (*(str + i))
+		len++, i++;
+	len++; /* add null terminator to length */
 
-	while (k < l)
+	duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
+
+	if (duplicate_str == NULL) /* validate memory */
+		return (NULL);
+
+	i = 0;
+	while (i < len)
 	{
-		if (k <= i)
-			s[k] = s1[k];
-
-		if (k >= i)
-		{
-			s[k] = s2[j];
-			j++;
-		}
-
-		k++;
+		*(duplicate_str + i) = *(str + i);
+		i++;
 	}
 
-	s[k] = '\0';
-	return (s);
+	return (duplicate_str);
 }
