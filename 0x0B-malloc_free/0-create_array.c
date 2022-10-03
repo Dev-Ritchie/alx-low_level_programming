0dcdbc38d0b43a4146f0f6757de6ab99b359943c
Dev-Ritchie <Ogbonnayaritchie@gmail.com>
2022-10-03 14:38:07 +0000

    No vex

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i = 0;
+
+	if (size <= 0) /* validate size input */
+		return (NULL);
+	arr = malloc(sizeof(char) * size); /* allocate memory */
+
+	if (arr == '\0') /* validate memory */
+		return (NULL);
+
+	while (i < (int)size) /* set array values to char c */
+	{
+		*(arr + i) = c;
+		i++;
+	}
+	*(arr + i) = '\0';
+
+	return (arr);
+}
