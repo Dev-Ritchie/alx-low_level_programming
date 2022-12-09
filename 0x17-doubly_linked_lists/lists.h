36052f63807d203ef81bd70f6eef095d459f1c96
Dev-Ritchie <Ogbonnayaritchie@gmail.com>
2022-12-09 21:53:23 +0000

    Garri

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h> /* printf */
-#include <stdlib.h> /* malloc, free */
+#include <stddef.h>
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 
 /**
  * struct dlistint_s - doubly linked list
@@ -20,7 +37,15 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/* function prototypes */
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void startupfun(void) __attribute__ ((constructor));
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -31,7 +56,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-/* helper functions */
-dlistint_t *insert_node(dlistint_t *tmp, int n);
-
-#endif
+#endif /* LISTS_H */
